ab2d33e75b9f375300234f5ad2933a7c94099196
James Hugman <james@hugman.tv>
2014-11-01 23:15:08 +0000

    Expose calculator-controller to kirin, and thus binding with the native screen.

--- config/extensions.js
+++ config/extensions.js
@@ -1,7 +1,8 @@
 module.exports = {
   aliases: {
     'keyboard-controller': './lib/app/keyboard-controller',
-    'editor-display-controller': './lib/app/editor-display-controller'
+    'editor-display-controller': './lib/app/editor-display-controller',
+    'calculator-controller': './lib/app/calculator-controller',
   },
   verbose: false
 };
